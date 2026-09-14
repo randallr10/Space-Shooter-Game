42882a4c6325a57ce6d094e1c2cc2cd1b0719825
randallr10 <ra.r.richard@baypath.net>
2026-09-14 20:00:31 +0200

    First Push - stage 1

---
+++ mainGame.py
@@ -0,0 +1,60 @@
+#import the pygame library 
+import pygame 
+
+#anchor the pygame screen so you see it in codio.
+#Click on the arrow in the upper left corner to display in a new browser tab.
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
+
+#start the pygame module 
+pygame.init() 
+
+#variables for screen size: 
+screen_width=1500
+screen_height=1000
+
+#color code constants 
+WHITE = (255,255,255)
+
+#other variable initializers (fonts, text, images, etc)
+
+
+#create a screen with dimensions 
+screen = pygame.display.set_mode((screen_width, screen_height)) 
+
+#set the screen caption 
+pygame.display.set_caption("Space Shooter")
+screen.fill((0, 0, 0))
+
+#the clock will be used to regulate the frame rate 
+clock = pygame.time.Clock() 
+
+#This function call updates the screen 
+pygame.display.update() 
+
+#sets the frame rate
+clock.tick(60) 
+
+#variable to control the game loop 
+keep_playing=True 
+
+#Game Loop - needed to keep updating and redrawing the screen 
+while keep_playing==True: 
+  #iterates over the current list of events(checks for events)  
+  for event in pygame.event.get(): 
+    #will stop the game loop if escape is pressed 
+    if event.type == pygame.QUIT: 
+      keep_playing = False
+      
+    #all items drawn to the screen go here
+    pygame.draw.line(screen, WHITE, [0,00], [500,600], 5)
+
+       
+  #This function call updates the screen 
+  pygame.display.update() 
+  #sets the frame rate
+  clock.tick(60) 
+
+#quits the pygame module 
+pygame.quit() 
+quit() 
