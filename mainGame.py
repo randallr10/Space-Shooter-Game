60aa70a91d66cd4fe17d034bc20af7a83515ac29
randallr10 <ra.r.richard@baypath.net>
2026-09-17 18:09:06 +0200

    Stage 4 done - enemy spawn

--- mainGame.py
+++ mainGame.py
@@ -1,5 +1,6 @@
 #import the pygame library 
 import pygame 
+import random
 
 #anchor the pygame screen so you see it in codio.
 #Click on the arrow in the upper left corner to display in a new browser tab.
@@ -16,26 +17,52 @@ screen_height=1000
 #color code constants 
 WHITE = (255,255,255)
 YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
 
 #other variables & functions
 player_x = 750
 bullet_y = 875
+enemy_y = 0
 SHOT_DELAY = 350
+ENEMY_DELAY = 1000
 last_shot = 0
+last_enemy = 0
 bullets = []
+enemies = []
 
+
+
+#creates the bullets and their coordinates
 def create_bullet(bullets, player_x, bullet_y):
   player_x = player_x + 20
   ammo = pygame.Rect(player_x, bullet_y, 10, 25)
   bullets.append(ammo)
   return bullets
 
+#creates the enemies and adds them to a list
+def create_enemies(enemies, enemy_y):
+  enemy_x = random.randint(0, 1450)
+  enemy = pygame.Rect(enemy_x, enemy_y, 50, 50)
+  enemies.append(enemy)
+  return enemies
+
+
+  #draws the player character
+def draw_player(player_x):
+  pygame.draw.rect(screen, WHITE, (player_x, 900, 50, 50))
+
+#draws the billets onto the screen based on the coordinate they were created on.
 def draw_bullets(bullets):
   for bullet in bullets:
     pygame.draw.rect(screen, YELLOW, bullet)
-        
+
+def draw_enemies(enemies):
+  for enemy in enemies:
+    pygame.draw.rect(screen, RED, enemy)
+
+#moves the player left and right without going off screen
 def move_player(player_x):
-  speed = 5
+  speed = 7.5
   if keys[pygame.K_LEFT]:
     if player_x > 0:
       player_x -= speed
@@ -44,15 +71,25 @@ def move_player(player_x):
       player_x += speed
   return player_x
 
-def move_bullets(bullets):
+#moves the bullets from the players location to the top of the screen and deletes it from the list once it reaches the top
+def move_bullets(bullets, enemies):
   speed = 10
   for bullet in bullets:
     bullet.y -= speed
     if bullet.bottom < 0:
       bullets.remove(bullet)
 
-def draw_player(player_x):
-  pygame.draw.rect(screen, WHITE, (player_x, 900, 50, 50))
+#moves the enemies down towards the player
+def move_enemies(enemies, bullets):
+  speed = 1
+  for enemy in enemies:
+    enemy.y += speed
+    for bullet in bullets:
+      if enemy.top > 1000 or enemy.colliderect(bullet):
+        enemies.remove(enemy)
+        bullets.remove(bullet)
+
+
 
 #create a screen with dimensions 
 screen = pygame.display.set_mode((screen_width, screen_height)) 
@@ -87,24 +124,36 @@ while keep_playing==True:
         if current_time - last_shot >= SHOT_DELAY:
           bullets = create_bullet(bullets, player_x, bullet_y)
           last_shot = current_time
-  
 
   #checks for keyboard input
   keys = pygame.key.get_pressed()
 
-  move_bullets(bullets)
+  enemy_time = pygame.time.get_ticks()
+  if enemy_time - last_enemy >= ENEMY_DELAY:
+    enemies = create_enemies(enemies, enemy_y)
+    last_enemy = enemy_time
+
+  move_bullets(bullets, enemies)
   for bullet in bullets:
     pygame.draw.rect(screen, YELLOW, bullet)
 
+
   screen.fill((0,0,0))
+
+
   player_x = move_player(player_x)
 
+  move_enemies(enemies, bullets)
+
   screen.fill((0, 0, 0))
+
 
   draw_bullets(bullets)
   
   draw_player(player_x)
-       
+
+  draw_enemies(enemies)
+
   #This function call updates the screen 
   pygame.display.update() 
   #sets the frame rate
